d496b01eb72c6840444b438f40574650e02c2927
Emerson <emersonline2007@gmail.com>
2018-10-14 20:22:01 -0400

    - Conteúdos de Aula do Curso de Desenvolvimento de Sofware - Conteúdos de Aula do Curso de Gestão de Segurança da Informação - Conteúdos de Aula do Curso de Qualidade de Sofware

--- Desenvolvimento de Software/Aula1-2/Aula1-2/Program.vb
+++ Desenvolvimento de Software/Aula1-2/Aula1-2/Program.vb
@@ -1,10 +1,18 @@
-﻿Module Program
-	Sub Main()
-		Console.WriteLine("Hello World!")
-		
-		' TODO: Implement Functionality Here
-		
-		Console.Write("Press any key to continue . . . ")
-		Console.ReadKey(True)
-	End Sub
+﻿Module Module1
+     Sub Main()
+         Dim idade As Integer
+         Dim c1 As Integer = 0
+         Dim c2 As Integer = 0
+         Console.WriteLine("Digite a idade:")
+         idade = Console.ReadLine()
+         While idade <> -1
+             If idade < 18 Then c1 = c1 + 1
+             If idade > 60 Then c2 = c2 + 1
+             Console.WriteLine("Digite a idade:")
+             idade = Console.ReadLine()
+         End While
+         Console.WriteLine(c1)
+         Console.WriteLine(c2)
+         Console.ReadKey()
+     End Sub
 End Module
